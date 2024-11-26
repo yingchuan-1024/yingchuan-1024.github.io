b79ebf72b6bb84d978172fb01a6c31ae6e0c9eaf
yingchuan-1024 <2822362038@qq.com>
2024-11-26 08:46:33 +0800

    Site updated: 2024-11-26 08:46:33

--- js/mouse.js
+++ js/mouse.js
@@ -1,24 +1,28 @@
+ 
 import { setANICursor } from 'ani-cursor.js';
 import { setANICursorWithGroupElement } from "ani-cursor.js";
- document.addEventListener('DOMContentLoaded', function() {
-      // 为<body>设置动画鼠标指针
-      setANICursor("body", "/img/ani/arrow.ani", "auto",width = 32,height = 32);
 
-      // 为<img>设置动画鼠标指针
-      setANICursor("img", "/img/ani/link.ani", "auto",width = 32,height = 32);
+document.addEventListener('DOMContentLoaded', function() {
+    // 为<body>设置动画鼠标指针
+    setANICursor("body", "/img/ani/arrow.ani", "auto", { width: 32, height: 32 });
 
-      // 为<p>设置动画鼠标指针
-      let textAbleGroup = [
-  "input",
-  'input[type="text"]',
-  "textarea",
-  "span",
-  "p",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-];
-setANICursorWithGroupElement(textAbleGroup, "/img/ani/beam.ani","auto",width = 32,height = 32);
+    // 为<img>设置动画鼠标指针
+    setANICursor("img", "/img/ani/link.ani", "auto", { width: 32, height: 32 });
+
+    // 为<p>设置动画鼠标指针
+    let textAbleGroup = [
+        "input",
+        'input[type="text"]',
+        "textarea",
+        "span",
+        "p",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+    ];
+    setANICursorWithGroupElement(textAbleGroup, "/img/ani/beam.ani", "auto", { width: 32, height: 32 });
+});
+ 
