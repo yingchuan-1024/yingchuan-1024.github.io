91bc04c4188b5f259e8f08ef6263ffd47803c676
yingchuan-1024 <2822362038@qq.com>
2024-12-06 11:48:06 +0800

    Site updated: 2024-12-06 11:48:05

--- js/mouse.js
+++ js/mouse.js
@@ -2,18 +2,26 @@ import { setANICursor } from "ani-cursor.js";
 import { setANICursorWithGroupElement } from "ani-cursor.js";
 
 // 为<body>设置动画鼠标指针
-setANICursor("body", "/img/ani/arrow.ani", "auto");
+setANICursor("body", "/img/ani/凯尔希指针/normal.ani", "auto", 50, 50);
 
 // 为<img>设置动画鼠标指针
-setANICursor("img", "/img/ani/link.ani", "auto");
+setANICursor("img", "/img/ani/凯尔希指针/cross.ani", "auto", 40, 40);
+
+//为触控设置动画鼠标指针
+let clickAbleGroup = [
+    "button",
+	"a",
+	"span",
+];
+setANICursorWithGroupElement(clickAbleGroup, "/img/ani/凯尔希指针/link.ani", "auto", 40, 40);
 
 // 为<p>设置动画鼠标指针
 let textAbleGroup = [
     "input",
     'input[type="text"]',
     "textarea",
-    "span",
     "p",
+	"i",
     "h1",
     "h2",
     "h3",
@@ -21,4 +29,4 @@ let textAbleGroup = [
     "h5",
     "h6",
 ];
-setANICursorWithGroupElement(textAbleGroup, "/img/ani/beam.ani");
+setANICursorWithGroupElement(textAbleGroup, "/img/ani/凯尔希指针/text.ani",);
