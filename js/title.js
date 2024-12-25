e9aa3c121803e190842d584e9a25fdbb28097f90
yingchuan-1024 <2822362038@qq.com>
2024-12-25 17:41:17 +0800

    Site updated: 2024-12-25 17:41:05

---
+++ js/title.js
@@ -0,0 +1,8 @@
+<script>
+document.addEventListener('visibilitychange', function () {
+if (document.visibilityState == 'hidden') {
+normal_title = document.title;
+document.title = '补药离开我 இωஇ';
+} else document.title = normal_title;
+});
+</script>
