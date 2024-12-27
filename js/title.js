5458116d44e28833ab6ab15edc1aa2489b8b6734
yingchuan-1024 <2822362038@qq.com>
2024-12-27 09:30:23 +0800

    Site updated: 2024-12-27 09:30:08

--- js/title.js
+++ js/title.js
@@ -1,8 +1,14 @@
-<script>
-document.addEventListener('visibilitychange', function () {
-if (document.visibilityState == 'hidden') {
-normal_title = document.title;
-document.title = '补药离开我 இωஇ';
-} else document.title = normal_title;
-});
-</script>
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = 'இωஇ 补药离开我~ ' + OriginTitile;
+            clearTimeout(titleTime);
+        }
+        else {
+            document.title = '(≧ω≦)/ 耶，肥来了~ ' + OriginTitile;
+            titleTime = setTimeout(function() {
+                document.title = OriginTitile;
+            }, 2000);
+        }
+    });
